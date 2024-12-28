53ab411021f581ec6587f8ae151e99f3ac4d3380
UmJi <147022364+userUmji@users.noreply.github.com>
2024-12-28 20:38:22 +0900

    인벤토리 추가 만들긴 했는데 실험 안해봄

--- PHPH/Assets/UmJi/Script/Inventory_S/Player_Inventory.cs
+++ PHPH/Assets/UmJi/Script/Inventory_S/Player_Inventory.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Player_Inventory : Inventory_Manager
 {
+    public static Player_Inventory instance;
+
     public List<Inven_Slot> slot_List;
     public List<Inven_Slot> godGet_List;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 }
