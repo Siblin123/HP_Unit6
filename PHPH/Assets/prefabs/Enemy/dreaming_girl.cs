628a999c887e4fac89084f5953ff903825d2e55a
jiho <wlgh7546@naver.com>
2025-01-14 23:59:53 +0900

    텔포 몬스터 , 총발싸 틀 제작

--- PHPH/Assets/prefabs/Enemy/dreaming_girl.cs
+++ PHPH/Assets/prefabs/Enemy/dreaming_girl.cs
@@ -1,34 +1,56 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class dreaming_girl : Enemy
 {
     //5. 찾는 범위안에 플레이어있으면 플레이어 방향으로 순간이동
     // 순간이동 범위내에 플레이어 있으면 몸박
+
     [Header("텔포 이동거리")]
     public float teleport_Range;
     public float teleportTime;
     [SerializeField]float curTeleportTime;
+    [Header("Ground 선택")]
+    public LayerMask find_Ground;
+
+    [Header("순간이동전에 미리 움직여보는 오브젝트")]
+    public GameObject ready_Move_Obj;
+    public bool isCorutineRun;
     private void Update()
     {
-        Teleport();
+        Find_Player();
     }
 
-    public void Teleport()
+    public override void Find_Player()
     {
         if (teleportTime <= curTeleportTime)
         {
-            Collider2D hit = Physics2D.OverlapCircle(transform.position, teleport_Range, find_layerMask);
+            Collider2D hit = Physics2D.OverlapCircle( transform.position, find_range, find_layerMask);
             //범위를 그려줬으면 좋겠어
 
             if (hit)
             {
-                transform.position = hit.transform.position;
-            }
-            else
-            {
-                int num = Random.Range(-1, 1);
-                Vector3 newpos = new Vector2(transform.position.x + num, transform.position.y);
-                transform.position=Vector2.MoveTowards(transform.position, newpos, 1f);
+
+                float distance = Vector2.Distance(transform.position, hit.transform.position);
+
+                //범위 안에 플레이어가 있어서 플레이어 몸박
+                if (distance <= teleport_Range)
+                    transform.position = hit.transform.position;
+                else//범위안에 플레이어가 없어서 플레이어방향으로 최대거리 이동이동
+                {
+                    Vector2 dir = hit.transform.position - transform.position;
+                    dir= dir.normalized;
+
+                    ready_Move_Obj.SetActive(true);
+                    ready_Move_Obj.transform.position = transform.position + (Vector3)(dir * teleport_Range);
+                    //1.플레이어 방향 최대 거리로 중력이 있는 오브젝트 소환
+                    if(!isCorutineRun)
+                        StartCoroutine(Teleport_PosCheck_Obj());
+                 
+
+                }
+
             }
 
             curTeleportTime = 0;
@@ -40,12 +62,45 @@ public class dreaming_girl : Enemy
 
     }
 
+
+    IEnumerator Teleport_PosCheck_Obj()
+    {
+
+        isCorutineRun = true;
+        while (true)
+        {
+        
+
+            //오브젝트가 멀리있으면 순간이동을 하지않는다
+            float distance = Vector2.Distance(transform.position, ready_Move_Obj.transform.position);
+            if (distance > teleport_Range)
+            {
+                ready_Move_Obj.SetActive(false);
+                break;
+            }
+
+            yield return null;
+
+            if (ready_Move_Obj.GetComponent<Rigidbody2D>().linearVelocityY == 0)
+            {
+                transform.position = ready_Move_Obj.transform.position;
+                ready_Move_Obj.SetActive(false);
+                break;
+            }
+        }
+
+        isCorutineRun = false;
+    }
+
+
     private void OnDrawGizmos()
     {
 
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, teleport_Range);
+        Gizmos.DrawWireSphere(transform.position, find_range);
 
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, teleport_Range);
     }
 
 
--- PHPH/Assets/prefabs/Enemy/shooter.cs
+++ PHPH/Assets/prefabs/Enemy/shooter.cs
@@ -3,14 +3,61 @@ using UnityEngine;
 public class shooter : Enemy
 {
     //6. 원거리 공격 , 조준 후 발싸 , 조준 시 공격방향이 보임  공격시 움직이지 않음  2초후 공격 
-    void Start()
+
+    [Header("투사체 관련")]
+    public GameObject projectile_Obj;
+    public float ShootTimer;
+    public float ShootRange;
+    float curShootTime;
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shoot()
     {
-        
+        //플레이어위치에 따른 좌우 
+
+        if (ShootTimer <= curShootTime)
+        {
+            Collider2D hit = Physics2D.OverlapCircle(transform.position, find_range, find_layerMask);
+            //범위를 그려줬으면 좋겠어
+
+            if (hit)
+            {
+
+                float distance = Vector2.Distance(transform.position, hit.transform.position);
+
+                //범위 안에 플레이어가 있어서 플레이어 몸박
+                if (distance <= ShootRange)
+                    transform.position = hit.transform.position;
+                else//범위안에 플레이어가 없어서 플레이어방향으로 최대거리 이동이동
+                {
+                    Vector2 dir = hit.transform.position - transform.position;
+                    dir = dir.normalized;
+
+                    projectile_Obj.SetActive(true);
+                    //총 발싸 되어야함
+                    
+
+                }
+
+            }
+
+            ShootTimer = 0;
+        }
+        else
+        {
+            ShootTimer += Time.deltaTime;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, find_range);
+
     }
 }
