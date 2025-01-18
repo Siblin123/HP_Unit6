666faa54693ded0a6c37ee93ea464b1a9f298d02
jiho <wlgh7546@naver.com>
2025-01-18 17:17:15 +0900

    임시저장

---
+++ PHPH/Assets/prefabs/OBJECT/Item/combination item/Consume_Item/Hearthstone.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Hearthstone : Item_Info
+{
+    public override void UseItem()
+    {
+        base.UseItem();
+        F_Hearthstone();
+    }
+
+    public void F_Hearthstone()
+    {
+        csTable.Instance.gameManager.player.curItem = null;
+        csTable.Instance.gameManager.player.transform.position = csTable.Instance.unitSopn_Pos.position;
+
+        //미니인벤토리에 있는 모든 아이템 버리기
+
+        //인벤토리에서 아이템 제거(파괴)
+    }
+}
