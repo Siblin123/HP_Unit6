5faa6ec58a1db416e33355d4338bd5062a4ffe91
UmJi <147022364+userUmji@users.noreply.github.com>
2025-01-25 22:30:37 +0900

    상점 연동 시도4

--- PHPH/Assets/UmJi/Script/Shop_S/Shop_Manager.cs
+++ PHPH/Assets/UmJi/Script/Shop_S/Shop_Manager.cs
@@ -110,7 +110,20 @@ public class Shop_Manager : interaction
         {
             inven_Slot_List[i].Update_Slot(csTable.Instance.gameManager.player.GetComponent<Player_Inventory>().slot_List[i].item, csTable.Instance.gameManager.player.GetComponent<Player_Inventory>().slot_List[i].have_Count);
 
-            csTable.Instance.gameManager.player.GetComponent<Player_Inventory>().Money_Slot_Find();
+            //csTable.Instance.gameManager.player.GetComponent<Player_Inventory>().Money_Slot_Find();
+
+            if (inven_Slot_List[i].item != null)
+            {
+                // id°¡ 100Àº µ·
+                if (inven_Slot_List[i].item.id == 100)
+                {
+                    money_Slot = inven_Slot_List[i];
+
+                    money = inven_Slot_List[i].have_Count;
+
+                    money_T.text = inven_Slot_List[i].have_Count.ToString("N0");
+                }
+            }
         }
     }
 
--- PHPH/Assets/prefabs/Manager/RPCmanager.cs
+++ PHPH/Assets/prefabs/Manager/RPCmanager.cs
@@ -5,8 +5,8 @@ using static UnityEditor.Progress;
 
 public class RPCmanager : NetworkBehaviour
 {
-    [ClientRpc]
-    public bool buy_Slot_ClientRpc(bool buy_C, Item_Info item) // 아이템 구매
+/*    [ClientRpc]
+    public void buy_Slot_ClientRpc(bool buy_C, Item_Info item) // 아이템 구매
     {
         // 구매 가능할때
         if (buy_C == true)
@@ -14,17 +14,11 @@ public class RPCmanager : NetworkBehaviour
             if (csTable.Instance.gameManager.player.GetComponent<Player_Inventory>().Buy_Item(item))
             {
                 Shop_Manager.instance.Invent_Shop();
-                return false;
             }
             else
             {
                 print("너님 돈 없어요");
-                return true;
             }
         }
-        else
-        {
-            return false;
-        }
-    }
+    }*/
 }
