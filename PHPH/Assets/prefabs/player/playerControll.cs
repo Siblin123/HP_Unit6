d4290f68f9fae1b0a496fc3ec7abf87fe32d5b83
UmJi <147022364+userUmji@users.noreply.github.com>
2025-02-25 17:53:39 +0900

    nomarge

--- PHPH/Assets/prefabs/player/playerControll.cs
+++ PHPH/Assets/prefabs/player/playerControll.cs
@@ -48,8 +48,11 @@ public class PlayerControl : PlayerStatus
         }
 
         if (IsServer)
+        {
             Shop_Manager.instance.Update_Slot();
             Shop_Manager.instance.List_Bring_ServerRpc();
+        }
+         
 
     }
 
